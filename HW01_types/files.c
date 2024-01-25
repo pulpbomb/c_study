81473de3729050d8dad1212f1353b78bc76a4815
pulpbomb <steamsuv@gmail.com>
2024-01-25 13:10:42 +0300

    for loop try

--- HW01_types/files.c
+++ HW01_types/files.c
@@ -5,7 +5,7 @@
 
 /*
 + 0. Находим eocdr, 0x504b0506, если нашли - это zip, можно работать, если нет, то выводим сообщение, что это не zip-файл.
-+ 1. Находим Central directory file header, 0x504b0102, от его начала 29-30 байты - значение длинны имени файла.
++ 1. Находим Central directory file header(cdfh), 0x504b0102, от его начала 29-30 байты - значение длинны имени файла.
 + 2. От начала Central directory file header, 47 байт - это начала имени файла, надо прочитать кол-во байт из переменной длинны файла и отдать в виде строки.
 - 3. Цикл, который будет каждый раз смещать указатель на следующий Central directory file header и повторять п.1 и п.2 пока не упремся в EOF или не исчерпаем кол-во total entries.
 ---- Кол-во total entries = кол-ву файлов в архиве, эта цифра мб кол-вом повторений цикла.
@@ -72,18 +72,15 @@ unsigned short get_total_enties(FILE *fd){
         return te;
 }
 
-long get_cdfh_filelen_offset(FILE *fd, ...){
-        size_t prev_filelen_offset;
-        fseek(fd, 0, SEEK_SET);
+long get_cdfh_filelen_offset(FILE *fd, long offset_filelen){
+        fseek(fd, offset_filelen, SEEK_SET);
         unsigned char cdfh_sign[] = {0x50, 0x4b, 0x01, 0x02};
         size_t pattern_len = sizeof(cdfh_sign) / sizeof(cdfh_sign[0]);
         unsigned char buffer[pattern_len];
         size_t bytes_read;
-        long offset_filelen = -1;
         while ((bytes_read = fread(buffer, 1, pattern_len, fd)) == pattern_len) {
                 if (memcmp(buffer, cdfh_sign, pattern_len) == 0) {
                 offset_filelen = ftell(fd) - pattern_len + 28;
-                prev_filelen_offset = offset_filelen;
                 break;
                 }
                 fseek(fd, -pattern_len + 1, SEEK_CUR);
@@ -92,12 +89,11 @@ long get_cdfh_filelen_offset(FILE *fd, ...){
                 puts("FILE_LEN Failed to read file!\n");
                 offset_filelen = -1;
         }
-        return prev_filelen_offset;
+        return offset_filelen;
 }
 
-long get_cdfh_filename_offset(FILE *fd, ...){
-        long prev_filelen_offset = get_cdfh_filelen_offset(fd);
-        fseek(fd, prev_filelen_offset, SEEK_SET);
+long get_cdfh_filename_offset(FILE *fd, long offset_filelen){
+        fseek(fd, offset_filelen, SEEK_SET);
         long offset_filename = ftell(fd) + 18;
         /*
         unsigned char cdfh_sign[] = {0x50, 0x4b, 0x01, 0x02};
@@ -121,11 +117,9 @@ long get_cdfh_filename_offset(FILE *fd, ...){
         return offset_filename;
 }
 
-char* get_filename_from_zip(FILE *fd){
-        long offset_filename = get_cdfh_filename_offset(fd);
-        long offset_filelen = get_cdfh_filelen_offset(fd);
+char* get_filename_from_zip(FILE *fd, long offset_filelen, long offset_filename){
 
-        if (offset_filename == -1 || offset_filelen == -1) {
+        if (offset_filelen == -1) {
                 return NULL;
         }
         // Считываем длину имени
@@ -157,23 +151,30 @@ int main(int argc, char **argv){
         long offset = get_eocdr_offset(fd);
         long offset_te = get_total_enties_offset(fd);
         int te = get_total_enties(fd);
-        long prev_filelen_offset = get_cdfh_filelen_offset(fd);
-        long offset_filename = get_cdfh_filename_offset(fd);
-        char  *zipfilename = get_filename_from_zip(fd);
-
+        long offset_filelen = 0;
         if (offset != -1) {
                 printf("EOCDR found, this is ZIP-file.\n");
-                printf("EOCDR must be at offset %ld\n", offset);
-                if (prev_filelen_offset == -1) {
+                printf("EOCDR must be at the offset %ld\n", offset);
+                printf("Total entries must be at the offset %ld\n", offset_te);
+                printf("Total entries - %d\n", te);
+                if (offset_filelen == -1) {
                 printf("But ZIP-file is empty!\n");
                 return 1;
                 }
-                //debug...
-                printf("Total entries must be at offset %ld\n", offset_te);
-                printf("Total entries - %d\n", te);
-                printf("FILELEN must be at offset %ld\n", prev_filelen_offset);
-                printf("FILENAME must be at offset %ld\n", offset_filename);
-                printf("Filenames = %s\n", zipfilename);
+                for(int i=0; i != 2; ++i){
+                        long new_offset_filelen = get_cdfh_filelen_offset(fd, offset_filelen);
+                        long offset_filelen = new_offset_filelen;
+                        printf("\n\n");
+                        printf("FILELEN must be at the offset %ld\n", new_offset_filelen);
+                        long offset_filename = get_cdfh_filename_offset(fd, offset_filelen);
+                        printf("FILENAME must be at the offset %ld\n", offset_filename);
+                        char  *zipfilename = get_filename_from_zip(fd, offset_filelen, offset_filename);
+                        printf("Filenames = %s\n", zipfilename);
+                        offset_filelen = offset_filename;
+                        printf("NEXT_FILELEN must be at the offset %ld\n", offset_filelen);
+                        get_cdfh_filelen_offset(fd, offset_filelen);
+                        printf("\n\n");
+                }
         }
         else {
                 printf("File has no zip signature!");
