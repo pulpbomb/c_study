67dc6770556d8c12b471187105671f819f44a1d0
pulpbomb <steamsuv@gmail.com>
2024-01-25 15:21:35 +0300

    strange things with buffer...

--- HW01_types/files.c
+++ HW01_types/files.c
@@ -3,21 +3,6 @@
 #include <string.h>
 #include <locale.h>
 
-/*
-+ 0. Находим eocdr, 0x504b0506, если нашли - это zip, можно работать, если нет, то выводим сообщение, что это не zip-файл.
-+ 1. Находим Central directory file header(cdfh), 0x504b0102, от его начала 29-30 байты - значение длинны имени файла.
-+ 2. От начала Central directory file header, 47 байт - это начала имени файла, надо прочитать кол-во байт из переменной длинны файла и отдать в виде строки.
-- 3. Цикл, который будет каждый раз смещать указатель на следующий Central directory file header и повторять п.1 и п.2 пока не упремся в EOF или не исчерпаем кол-во total entries.
----- Кол-во total entries = кол-ву файлов в архиве, эта цифра мб кол-вом повторений цикла.
----- Для начала цикла есть:
-        a. Смещение поля имени первого файла.
-        b. Смещение поля длинны имени первого файла.
-        c. Длинна имени первого файла.
-        d. Эти значения надо сохранить в переменные внутри цикла. 
-        e. В конце итеррации цикла эти смещения надо как-то обновлять...обновятся ли они для сторонних фукнций из которых ранее эти смещения и получили, если внутри цикла эти функции вызывать?
-        - вроде бы fread двигает указатель после считывания сам, это где-то само сохраняется или вне функции с fread это стоит запоминать отдельно?
-        f. Цикл должен записывать имя переменной (возвращаемый результат из get_filename_from_zip) видимо в динамический массив?
-*/
 long get_eocdr_offset(FILE *fd){
         fseek(fd, 0, SEEK_SET);
         unsigned char eocdr_sign[] = {0x50, 0x4b, 0x05, 0x06};
@@ -73,11 +58,12 @@ unsigned short get_total_enties(FILE *fd){
 }
 
 long get_cdfh_filelen_offset(FILE *fd, long offset_filelen){
-        fseek(fd, offset_filelen, SEEK_SET);
+        fseek(fd, offset_filelen, SEEK_SET);    
         unsigned char cdfh_sign[] = {0x50, 0x4b, 0x01, 0x02};
         size_t pattern_len = sizeof(cdfh_sign) / sizeof(cdfh_sign[0]);
         unsigned char buffer[pattern_len];
         size_t bytes_read;
+        //Тут происходит что-то непонятное, на второй итерации цикла в main кусок кода ниже превращается в тыкву...
         while ((bytes_read = fread(buffer, 1, pattern_len, fd)) == pattern_len) {
                 if (memcmp(buffer, cdfh_sign, pattern_len) == 0) {
                 offset_filelen = ftell(fd) - pattern_len + 28;
@@ -92,28 +78,9 @@ long get_cdfh_filelen_offset(FILE *fd, long offset_filelen){
         return offset_filelen;
 }
 
-long get_cdfh_filename_offset(FILE *fd, long offset_filelen){
-        fseek(fd, offset_filelen, SEEK_SET);
+long get_cdfh_filename_offset(FILE *fd, long new_offset_filelen){
+        fseek(fd, new_offset_filelen, SEEK_SET);
         long offset_filename = ftell(fd) + 18;
-        /*
-        unsigned char cdfh_sign[] = {0x50, 0x4b, 0x01, 0x02};
-        size_t pattern_len = sizeof(cdfh_sign) / sizeof(cdfh_sign[0]);
-        unsigned char buffer[pattern_len];
-        size_t bytes_read;
-        long offset_filename = -1;
-        while ((bytes_read = fread(buffer, 1, pattern_len, fd)) == pattern_len) {
-                if (memcmp(buffer, cdfh_sign, pattern_len) == 0) {
-                offset_filename = ftell(fd) - pattern_len + 18;
-                break;
-                }
-                fseek(fd, -pattern_len + 1, SEEK_CUR);
-        }
-
-        if (bytes_read != pattern_len && !feof(fd)) {
-                puts("ZIP_FNAME Failed to read file!\n");
-                offset_filename = -1;
-        }
-        */
         return offset_filename;
 }
 
@@ -158,21 +125,20 @@ int main(int argc, char **argv){
                 printf("Total entries must be at the offset %ld\n", offset_te);
                 printf("Total entries - %d\n", te);
                 if (offset_filelen == -1) {
-                printf("But ZIP-file is empty!\n");
-                return 1;
+                                printf("But ZIP-file is empty!\n");
+                                return 1;
                 }
-                for(int i=0; i != 2; ++i){
+                for(int i=0; i != 2; i++){
+                        printf("OLD_FILELEN must be at the offset %ld\n", offset_filelen);
                         long new_offset_filelen = get_cdfh_filelen_offset(fd, offset_filelen);
-                        long offset_filelen = new_offset_filelen;
                         printf("\n\n");
                         printf("FILELEN must be at the offset %ld\n", new_offset_filelen);
-                        long offset_filename = get_cdfh_filename_offset(fd, offset_filelen);
+                        long offset_filename = get_cdfh_filename_offset(fd, new_offset_filelen);
                         printf("FILENAME must be at the offset %ld\n", offset_filename);
-                        char  *zipfilename = get_filename_from_zip(fd, offset_filelen, offset_filename);
+                        char  *zipfilename = get_filename_from_zip(fd, new_offset_filelen, offset_filename);
                         printf("Filenames = %s\n", zipfilename);
                         offset_filelen = offset_filename;
                         printf("NEXT_FILELEN must be at the offset %ld\n", offset_filelen);
-                        get_cdfh_filelen_offset(fd, offset_filelen);
                         printf("\n\n");
                 }
         }
