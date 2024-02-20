5af3a192d0f41fff94f9929b84ca0b5c9cbd313b
pulpbomb <steamsuv@gmail.com>
2024-02-20 20:39:31 +0300

    new hws

--- HW04_libs/weather.c
+++ HW04_libs/weather.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,10 +6,87 @@
 #include <cjson/cJSON.h>
 #include <cjson/cJSON_Utils.h>
 
-int main(int argc, char* argv[]) {
+struct MemoryStruct {
+        char *memory;
+        size_t size;
+};
+ 
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp){
+        size_t realsize = size * nmemb;
+        struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+ 
+        char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+        if(!ptr) {
+                /* out of memory! */
+                printf("not enough memory (realloc returned NULL)\n");
+                return 0;
+        }
+ 
+        mem->memory = ptr;
+        memcpy(&(mem->memory[mem->size]), contents, realsize);
+        mem->size += realsize;
+        mem->memory[mem->size] = 0;
+ 
+        return realsize;
+}
+ 
+int main(int argc, char* argv[]){
         if (argc != 2) {
                 printf("Usage: %s <cityname>\n", argv[0]);
                 return errno;
         }
+        
+        CURL *curl_handle;
+        CURLcode res;
+ 
+        struct MemoryStruct chunk;
+ 
+        chunk.memory = malloc(1); /* will be grown as needed by the realloc above */
+        chunk.size = 0;           /* no data at this point */
+ 
+        curl_global_init(CURL_GLOBAL_ALL);
+ 
+        /* init the curl session */
+        curl_handle = curl_easy_init();
+ 
+        /* specify URL to get */
+        curl_easy_setopt(curl_handle, CURLOPT_URL, "https://wttr.in/Moscow");
+ 
+        /* send all data to this function  */
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+ 
+        /* we pass our 'chunk' struct to the callback function */
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+ 
+        /* some servers do not like requests that are made without a user-agent
+        field, so we provide one */
+        curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+ 
+        /* get it! */
+        res = curl_easy_perform(curl_handle);
+ 
+        /* check for errors */
+        if(res != CURLE_OK) {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+        }
+        else {
+                /*
+                * Now, our chunk.memory points to a memory block that is chunk.size
+                * bytes big and contains the remote file.
+                *
+                * Do something nice with it!
+                */
+                printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
+        }
+ 
+        /* cleanup curl stuff */
+        curl_easy_cleanup(curl_handle);
+ 
+        free(chunk.memory);
+ 
+        /* we are done with libcurl, so clean it up */
+        curl_global_cleanup();
+ 
         return 0;
 }
